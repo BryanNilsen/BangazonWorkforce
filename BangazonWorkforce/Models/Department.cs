af4273d4b89db960a33594f1bbb071e065c8d9ca
BryanNilsen <bryannilsen@gmail.com>
2019-11-15 14:01:16 -0600

    initial commit

---
+++ BangazonWorkforce/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models
+{
+    public class Department
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Budget { get; set; }
+    }
+}
---
+++ BangazonWorkforce/Models/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Is Supervisor")]
+        public bool IsSupervisor { get; set; }
+        [Required]
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+
+        public string SupervisorDisplay
+        {
+            get
+            {
+
+            if (this.IsSupervisor)
+            {
+                return "Yep";
+            }
+            return "Nope";
+
+            }
+        }
+    }
+}
